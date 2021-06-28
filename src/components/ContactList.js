ead5ad420f0a7c201fecb15a1df26e39a59f3a94
Gabe Ng <gng10836@gmail.com>
2021-06-28 13:48:02 -0400

    commit on 'main'

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
-    console.log(props);
-
+    const inputEl = useRef("");
     const deleteContactHandler = (id) => {
         props.getContactId(id);
     };
@@ -14,6 +13,11 @@ const ContactList = (props) => {
             <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id}/>
         );
     });
+
+    const getSearchTerm = () => {
+        props.searchKeyword(inputEl.current.value);
+    };
+    
     return (
     <div class="main">
         <h2>
@@ -23,8 +27,20 @@ const ContactList = (props) => {
             </Link>
             
         </h2>
-        
-        <div className="ui celled list">{renderContactList}</div>
+        <div className="ui search">
+            <div className="ui icon input">
+                <input 
+                    ref={inputEl} 
+                    type="text" 
+                    placeholder="Search Contacts" 
+                    className="prompt" 
+                    value={props.term} 
+                    onChange={getSearchTerm} 
+                />
+                <i classname="search icon"></i>
+            </div>
+        </div>
+        <div className="ui celled list">{renderContactList.length > 0 ? renderContactList : "No Contacts Available"}</div>
     </div>
     );
     
